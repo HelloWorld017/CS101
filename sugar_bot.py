b7d795f5eb0d36e63a404507970f98efe0ebc964
Khinenw <deu07115@gmail.com>
2020-09-22 00:05:02 +0900

    Add more sugars

--- sugar_bot.py
+++ sugar_bot.py
@@ -1,4 +1,7 @@
 # A bot with sugars
+# ====
+# Originally developed for doing lab when I was in the class
+# Updated for TA works
 
 class SugarBot(Robot):
     def __init__(self, *args, **kwargs):
@@ -36,12 +39,31 @@ class SugarBot(Robot):
         self.pick_until()
     
     def move_pick_until(self):
+        self.pick_until()
         while self.front_is_clear():
             self.move_pick()
         
     def ensure_north(self):
         while not self.facing_north():
             self.turn_left()
+    
+    def direction_oddeven(self, direction = 0):
+        return 1 if direction == 0 else 3
+    
+    def uturn(self, direction = 0):
+        self.turn_left_n(self.direction_oddeven(direction))
+        if not self.front_is_clear():
+            return False
+        
+        self.move()
+        self.turn_left_n(self.direction_oddeven(direction))
+        return True
+    
+    def turn_right(self):
+        self.turn_left_n(3)
+    
+    def turn_backward(self):
+        self.turn_left_n(2)
     
     def __getattr__(self, name):
         if not name.endswith("_n"):
